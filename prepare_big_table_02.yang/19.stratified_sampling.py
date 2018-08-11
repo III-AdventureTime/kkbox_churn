82c839b9c46bf3c4a408d0624755e445135591b1
Ming-Hsu Yang <bohdisvaha@hotmail.com>
2018-08-11 19:26:55 +0800

    updated create_features_from_transactions_v1.yang and prepare_big_table_02.yang

--- prepare_big_table_02.yang/19.stratified_sampling.py
+++ prepare_big_table_02.yang/19.stratified_sampling.py
@@ -6,16 +6,25 @@ if __name__ == "__main__":
     dir0 = 'file:///home/cloudera/2.kkbox_churn/data01/big_table_01/'
     infile = dir0 + '12.big_table_clean2'
     
-    outfile_train = dir0 + '19a.train_set'
-    outfile_test = dir0 + '19b.test_set'
-
+    filter_has_expired = True
     sample_ratio = 0.8
     use = 'my_is_churn'  # 'my_is_churn' or 'is_churn'
+
+    #
+    subdir = 'has_expired/' if filter_has_expired else ''
+    outfile_train = dir0 + subdir + '19a.train_set'
+    outfile_test = dir0 + subdir + '19b.test_set'
+
 
     ##
     spark = SparkSession.builder.getOrCreate()
     df0 = spark.read.format('parquet').load(infile)
     
+    ##
+    if filter_has_expired:
+        df0 = df0.where('has_expired')
+    
+    ##
     df_train = df0.sampleBy(use, {True: sample_ratio, False: sample_ratio}, seed=101207)
     df_test = df0.join(df_train, df0['msno']==df_train['msno'], 'left_anti')
     
